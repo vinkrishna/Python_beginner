4e87d270234da727a73bb6fb3308b5a84c8cfb4c
vinkrishna <vinod.bioinfo@gmail.com>
2018-12-25 14:52:22 +0100

    Extra functions/Classes

---
+++ Chef.py
@@ -0,0 +1,9 @@
+class Chef:
+    def make_chicken(self):
+        print("The chef makes noodles")
+        
+    def make_salad(self):
+        print("The chef cooks salad")
+        
+    def make_special_dish(self):
+        print("The chef cooks paneer")
